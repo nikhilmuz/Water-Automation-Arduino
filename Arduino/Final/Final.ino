77243ab9e7781d3d6a0a9d4c0c0e153e319a8a57
Aakif <vasidaakif@gmail.com>
2018-03-12 00:50:21 +0530

    Added final project

---
+++ Arduino/Final/Final.ino
@@ -0,0 +1,107 @@
+#include <EEPROM.h>
+int a0 = A0,a1 = A1,a2 = A2,a3 = A3,a4 = A4,a5 = A5;
+int va0,va1,va2,va3,va4,va5;
+char ba1,ba2,ba3,ba4;
+
+String actionsec,minmax;
+int min,max;
+char s1,s2,s3,s4;
+
+//eeprom start
+int writeaddr = 0;
+int readaddr = 0;
+void writeeeprom(String str){
+  EEPROM.update(writeaddr,str.length());
+  writeaddr++;
+  for(int i=0;i<str.length();i++){
+    EEPROM.update(writeaddr,str[i]);
+    writeaddr++;
+  }
+}
+String readeeprom(){
+  String output="";
+  readaddr++;
+  int len = EEPROM.read(readaddr-1);
+  for(int i=0;i<len;i++){
+    output+=(char)EEPROM.read(readaddr);
+    readaddr++;
+    }
+  return output;
+}
+//eeprom end
+
+char preftooutput(char pref){
+  if(pref=='1'){return HIGH;}
+  else if(pref=='0'){return LOW;}
+  return LOW;
+}
+
+void setup() {
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+Serial.begin(115200);
+writeaddr = 0; actionsec="2,2,2,2"; minmax="000,100";  writeeeprom(actionsec); writeeeprom(minmax);
+va1=(1024 - analogRead(a1))/10.24;
+va2=(1024 - analogRead(a2))/10.24;
+va3=(1024 - analogRead(a3))/10.24;
+va4=(1024 - analogRead(a4))/10.24;
+readaddr = 0;
+actionsec=readeeprom();
+minmax=readeeprom();
+min=(minmax.substring(0,3)).toInt();
+max=(minmax.substring(4,7)).toInt();
+ba1=ba2=ba3=ba4='1';
+if(va1>min&&va1<max){ba1='0';}
+if(va2>min&&va1<max){ba2='0';}
+if(va3>min&&va1<max){ba3='0';}
+if(va4>min&&va1<max){ba4='0';}
+}
+
+void loop(){
+va0=analogRead(a0);
+va1=(1024 - analogRead(a1))/10.24;
+va2=(1024 - analogRead(a2))/10.24;
+va3=(1024 - analogRead(a3))/10.24;
+va4=(1024 - analogRead(a4))/10.24;
+va5=analogRead(a5);
+  
+if(EEPROM.read(0)!=7){writeaddr = 0; actionsec="0,0,0,0"; minmax="000,100";  writeeeprom(actionsec); writeeeprom(minmax);}
+readaddr = 0;
+actionsec=readeeprom();
+minmax=readeeprom();
+
+s1=actionsec[0];
+s2=actionsec[2];
+s3=actionsec[4];
+s4=actionsec[6];
+
+min=(minmax.substring(0,3)).toInt();
+max=(minmax.substring(4,7)).toInt();
+
+if(s1!='2'){digitalWrite(9,preftooutput(s1));}
+if(s2!='2'){digitalWrite(10,preftooutput(s2));}
+if(s3!='2'){digitalWrite(12,preftooutput(s3));}
+if(s4!='2'){digitalWrite(13,preftooutput(s4));}
+
+//automation
+if(s1=='2'){if(va1<min){digitalWrite(9,preftooutput('1'));}
+else if(va1>max){digitalWrite(9,preftooutput('0'));}
+else{digitalWrite(9,preftooutput(ba1));}
+}
+if(s2=='2'){if(va2<min){digitalWrite(10,preftooutput('1'));}
+else if(va2>max){digitalWrite(10,preftooutput('0'));}
+else{digitalWrite(10,preftooutput(ba2));}
+}
+if(s3=='2'){if(va3<min){digitalWrite(12,preftooutput('1'));}
+else if(va3>max){digitalWrite(12,preftooutput('0'));}
+else{digitalWrite(12,preftooutput(ba3));}
+}
+if(s4=='2'){if(va4<min){digitalWrite(13,preftooutput('1'));}
+else if(va4>max){digitalWrite(13,preftooutput('0'));}
+else{digitalWrite(13,preftooutput(ba4));}
+}
+//automation end
+
+}
